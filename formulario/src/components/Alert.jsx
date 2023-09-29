ace7cde7b6090b15c4ca088ccaf3ef48c127aed8
Ayxa Chaverra <achaverrar@gmail.com>
2023-09-29 16:54:43 -0500

    fix typos in the Formulario and Alert components that preventing the adequate form handling and validation

--- formulario/src/components/Alert.jsx
+++ formulario/src/components/Alert.jsx
@@ -1,6 +1,6 @@
 const Alert = ({mensaje,color}) => {
     return (
-        <div className={`alert  alert -${color} mt-3`} role="alert">{mensaje} </div>
+        <div className={`alert  alert-${color} mt-3`} role="alert">{mensaje} </div>
     )
 }
 export default Alert
